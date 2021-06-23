dc766ef824c1461c926d33875106ba48548e1003
nata-mendes <natadasilvamendes@gmail.com>
2021-06-23 15:20:46 -0300

    Exercicios da parte III do conteudo

--- fundamentals/4.1-javascript-primeiros-passos/scripts.js
+++ fundamentals/4.1-javascript-primeiros-passos/scripts.js
@@ -1,8 +1,36 @@
-let name = "Natã";
-const birthCity = "Rio de Janeiro";
-let birthYear = 1999;
-birthYear = 2030;
+//Parte I do conteudo:
 
-console.log(name);
-console.log(birthCity);
-console.log(birthYear);
+// let name = "Natã";
+// const birthCity = "Rio de Janeiro";
+// let birthYear = 1999;
+// birthYear = 2030;
+
+// console.log(name);
+// console.log(birthCity);
+// console.log(birthYear);
+
+//Parte II do conteudo:
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+// console.log(typeof patienAge);
+
+let base = 5;
+let altura = 8;
+let area = base * altura
+
+console.log(area);
+
+let perimetro = (base + altura) * 2;
+
+console.log(perimetro);
