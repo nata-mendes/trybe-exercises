9dd5ede6ed435ecbe4fa3f8f6a7814e555b6586c
nata-mendes <natadasilvamendes@gmail.com>
2021-06-23 15:34:20 -0300

    Exercicios da parte IV do conteudo

--- fundamentals/4.1-javascript-primeiros-passos/scripts.js
+++ fundamentals/4.1-javascript-primeiros-passos/scripts.js
@@ -25,12 +25,26 @@
 // console.log(typeof patientEmail);
 // console.log(typeof patienAge);
 
-let base = 5;
-let altura = 8;
-let area = base * altura
+// let base = 5;
+// let altura = 8;
+// let area = base * altura
 
-console.log(area);
+// console.log(area);
 
-let perimetro = (base + altura) * 2;
+// let perimetro = (base + altura) * 2;
 
-console.log(perimetro);
+// console.log(perimetro);
+
+//Parte IV do conteudo:
+
+let nota = 1;
+
+if (nota >= 80) {
+  console.log("Parabéns! Você foi aprovada(o)!");
+}
+else if (nota < 80 && nota >= 60) {
+  console.log ("Você está na lista de espera.");
+}
+else {
+  console.log("Você foi reprovada(o).");
+}
