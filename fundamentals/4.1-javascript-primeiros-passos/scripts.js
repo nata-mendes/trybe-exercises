93126b518976e627024f9ed85d5913407cadce45
nata-mendes <natadasilvamendes@gmail.com>
2021-06-23 19:00:26 -0300

    Conclusao do exercicio 3

--- fundamentals/4.1-javascript-primeiros-passos/scripts.js
+++ fundamentals/4.1-javascript-primeiros-passos/scripts.js
@@ -87,11 +87,25 @@
 
 // Exercicio 02:
 
-let num1 = 10;
-let num2 = 18;
+// let num1 = 10;
+// let num2 = 18;
 
-if (num1 > num2) {
+// if (num1 > num2) {
+//   console.log(num1);
+// } else {
+//   console.log(num2);
+// }
+
+// Exercicio 03:
+
+let num1 = 55;
+let num2 = 33;
+let num3 = 25;
+
+if (num1 > num2 && num1 > num3) {
   console.log(num1);
-} else {
+} else if (num2 > num1 && num2 > num3) {
   console.log(num2);
+} else {
+  console.log(num3);
 }
