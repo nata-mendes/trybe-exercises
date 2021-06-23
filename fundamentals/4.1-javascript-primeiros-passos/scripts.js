54adc3394d27006cb182113583300d58a0eacd72
nata-mendes <natadasilvamendes@gmail.com>
2021-06-23 19:20:47 -0300

    Conclusao do exercicio 5

--- fundamentals/4.1-javascript-primeiros-passos/scripts.js
+++ fundamentals/4.1-javascript-primeiros-passos/scripts.js
@@ -124,3 +124,17 @@
 
 // Exercicio 05:
 
+// let ang1 = 120;
+// let ang2 = 60;
+// let ang3 = -60;
+
+// if (ang1 > 0 && ang2 > 0 && ang3 > 0 && (ang1 + ang2 + ang3) === 180) {
+//   console.log("true");
+// } else if (ang1 > 0 && ang2 > 0 && ang3 > 0 && (ang1 + ang2 + ang3) !== 180) {
+//   console.log("false");
+// } else if (ang1 < 0 || ang2 < 0 || ang3 < 0) {
+//   console.log("Um dos ângulos digitados tem um valor negativo. Favor verificar.");
+// }
+
+// Exercicio 06:
+
