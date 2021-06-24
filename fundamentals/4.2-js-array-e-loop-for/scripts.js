c814c335b66ca743a4d036a8c1707231203bb40a
nata-mendes <natadasilvamendes@gmail.com>
2021-06-24 16:15:12 -0300

    Exercicios 3 e 4 da parte de exercicios

--- fundamentals/4.2-js-array-e-loop-for/scripts.js
+++ fundamentals/4.2-js-array-e-loop-for/scripts.js
@@ -53,10 +53,29 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // 2:
 
-let sum = 0;
+// let sum = 0;
 
-for (let number of numbers) {
-  sum += number;
-}
+// for (let number of numbers) {
+//   sum += number;
+// }
 
-console.log(sum);
+// console.log(sum);
+
+// 3 e 4:
+
+// let sum = 0;
+
+// for (let number of numbers) {
+//   sum += number;
+// }
+
+// let media = sum / numbers.length;
+
+// console.log(media);
+
+// if (media > 20) {
+//   console.log('Valor maior que 20!');
+// } else {
+//   console.log('Valor menor ou igual a 20!');
+// }
+
