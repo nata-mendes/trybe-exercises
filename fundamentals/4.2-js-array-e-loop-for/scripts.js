b2d06c94c4cc399cc71379bd95b2b568dd857494
nata-mendes <natadasilvamendes@gmail.com>
2021-06-24 15:59:38 -0300

    Conclusao dos exercicios da parte de conteudos

--- fundamentals/4.2-js-array-e-loop-for/scripts.js
+++ fundamentals/4.2-js-array-e-loop-for/scripts.js
@@ -16,7 +16,27 @@
 
 // exercicio 3 da parte do conteudo:
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push = 'Contato';
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push = 'Contato';
 
-console.log(menu);
+// console.log(menu);
+
+// for
+
+// exercicio 1 da parte do conteudo:
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let index = 0; index < groceryList.length; index += 1) {
+//   console.log(groceryList[index]);
+// }
+
+// for/of:
+
+// exercicio 1 da parte do conteudo:
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+  console.log(name);
+}
