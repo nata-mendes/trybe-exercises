382bb9352d2b70b113af721ea8bc867848b7528e
nata-mendes <natadasilvamendes@gmail.com>
2021-06-24 15:24:23 -0300

    Conclusao do exercicio 2 da parte de arrays

--- fundamentals/4.2-js-array-e-loop-for/scripts.js
+++ fundamentals/4.2-js-array-e-loop-for/scripts.js
@@ -2,7 +2,15 @@
 
 // exercicio 1 da parte do conteudo:
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu.indexOf('Serviços');
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu.indexOf('Serviços');
 
-console.log(menuServices);
+// console.log(menuServices);
+
+// exercicio 2 da parte do conteudo:
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
