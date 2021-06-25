f4c14ee911f0d8a143ca1f793c88a5f86bb6d40d
nata-mendes <natadasilvamendes@gmail.com>
2021-06-24 22:29:59 -0300

    Inicio do exercicio 5

--- fundamentals/4.2-js-array-e-loop-for/scripts.js
+++ fundamentals/4.2-js-array-e-loop-for/scripts.js
@@ -79,3 +79,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //   console.log('Valor menor ou igual a 20!');
 // }
 
+// 5:
+
+let higherNumber;
+
+for (i = 0; i < numbers.length; i += 1) {
+
+  if ()
+
+}
