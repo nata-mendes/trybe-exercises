2527cc839b23954b65482da5f4ae53c4fa38ceea
nata-mendes <natadasilvamendes@gmail.com>
2021-06-28 15:57:31 -0300

    Conlusao dos exercicios de fixacao da parte do conteudo sobre for/in

--- fundamentals/4.4-objetos-e-funcoes/scripts.js
+++ fundamentals/4.4-objetos-e-funcoes/scripts.js
@@ -2,24 +2,43 @@
 
 // Parte do conteudo - Objetos:
 
-let player = {
-  name: 'Marta',
-  lastName: 'Silva',
-  age: 34,
-  medals: {
-    golden: 2,
-    silver: 3,
-  }
-};
+// let player = {
+//   name: 'Marta',
+//   lastName: 'Silva',
+//   age: 34,
+//   medals: {
+//     golden: 2,
+//     silver: 3,
+//   }
+// };
 
-console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade.`);
+// console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade.`);
 
-player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
-player.fullName = `${player.name} ${player.lastName}`;
+// player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+// player.fullName = `${player.name} ${player.lastName}`;
 
-console.log(`A jogadora ${player.fullName} foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes.`);
+// console.log(`A jogadora ${player.fullName} foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes.`);
 
-console.log(`A jogadora possui ${player.medals.golden} medalhas de ouro e ${player.medals.silver} medalhas de prata.`);
+// console.log(`A jogadora possui ${player.medals.golden} medalhas de ouro e ${player.medals.silver} medalhas de prata.`);
 
 // Parte do conteudo - For/in:
 
+// let names = {
+//   person1: 'João',
+//   person2: 'Maria',
+//   person3: 'Jorge' 
+// };
+
+// for (let persons in names) {
+//   console.log(`Olá, ${names[persons]}!`);
+// }
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let key in car) {
+  console.log(`${key}: ${car[key]};`);
+}
